8fdbf03bda49b3001f6c06a5c0df37044f4f8a68
Jesse Palmer <jesselpalmer@gmail.com>
2022-08-25 14:58:50 -0700

    chore: made updates

--- Contract.sol
+++ Contract.sol
@@ -3,9 +3,20 @@ pragma solidity >=0.5.0 <0.6.0;
 contract MrMarketFactory {
 
   uint mrMarketDigits = 100;
-  uint mrMarketModulus = 10 ** dnaDigits;
+  uint mrMarketModulus = 10 ** mrMarketDigits;
 
-  constructor() {
-    
+  struct MrMarket {
+    string name;
+    uint id;
+  }
+
+  MrMarket[] public mrMarkets;
+
+  function _createMrMarket(string memory _name, uint _id) private {
+    mrMarkets.push(MrMarket(_name, _dna));        
+  }
+
+  function _generateRandomId(string memory _str) private view returns (uint) {
+
   }
 }
