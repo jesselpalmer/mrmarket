d162f369be16483cfad36b28a4a85d30b3cd4535
Jesse Palmer <jesselpalmer@gmail.com>
2022-09-17 23:07:26 -0700

    chore: add code

--- contracts/MrMarketFactory.sol
+++ contracts/MrMarketFactory.sol
@@ -2,7 +2,16 @@
 
 pragma solidity ^0.8.17;
 
-contract MrMarketFactory {
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+
+contract MrMarketFactory is ERC721URIStorage {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() ERC721 ('MrMarket', 'MRMARKET') {
+
+  }
 
   event NewMrMarket(uint id, string name, uint dna);
 
